4e1c8534dfc9ac23549321372a5066156be2d110
Douglas Alexandre <161042865+Titanzada@users.noreply.github.com>
2024-09-12 19:50:27 -0300

    Delete Atividade Pontuada - Presença directory

--- Atividade Pontuada - Presença/models/Pessoa.py
+++
@@ -1,9 +0,0 @@
-from models.Endereco import Endereco
-from abc import ABC, abstractmethod
-
-class Pessoa(ABC):
-    def __init__(self, nome:str, telefone:str, email:str, endereco:Endereco) -> None:
-        self.nome = nome
-        self.telefone = telefone
-        self.email = email
-        self.endereco = endereco
